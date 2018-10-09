1ec5344eb7ef9020913ce449a4622be2ba00b3f3
Connor <connor.cooper@uqconnect.edu.au>
2018-10-09 12:34:13 +1000

    Added database setup script

---
+++ user_db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `users`
+(
+ `uuid`               VARCHAR(36) NOT NULL ,
+ `username`           VARCHAR(255) NOT NULL ,
+ `password`           VARCHAR(255) NOT NULL ,
+ `mem_times_played`   INT NOT NULL ,
+ `mem_average_time`   INT NOT NULL ,
+ `mem_best_time`      INT NOT NULL ,
+ `quiz_times_played`  INT NOT NULL ,
+ `quiz_average_score` FLOAT NOT NULL ,
+ `quiz_best_score`    INT NOT NULL ,
+ `languages_visited`  INT NOT NULL ,
+
+PRIMARY KEY (`uuid`)
+);
