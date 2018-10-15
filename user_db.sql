38ac3994b7f1215af5f942bdaed272789d50b65e
Connor <connor.cooper@uqconnect.edu.au>
2018-10-16 08:53:48 +1000

    Update user database for enchanced security options

--- user_db.sql
+++ user_db.sql
@@ -1,15 +1,17 @@
 CREATE TABLE `users`
 (
- `email`              VARCHAR(255) NOT NULL,
- `username`           VARCHAR(255) NOT NULL ,
- `password`           VARCHAR(255) NOT NULL ,
- `mem_times_played`   INT NOT NULL ,
- `mem_average_time`   INT NOT NULL ,
- `mem_best_time`      INT NOT NULL ,
- `quiz_times_played`  INT NOT NULL ,
- `quiz_average_score` FLOAT NOT NULL ,
- `quiz_best_score`    INT NOT NULL ,
- `languages_visited`  INT NOT NULL ,
+    `email`              VARCHAR(255) NOT NULL,
+    `username`           VARCHAR(255) NOT NULL ,
+    `password`           VARCHAR(255) NOT NULL ,
+    `time` DATETIME NOT NULL,
+    `attempts` INT NOT NULL,
+    `mem_times_played`   INT NOT NULL ,
+    `mem_average_time`   INT NOT NULL ,
+    `mem_best_time`      INT NOT NULL ,
+    `quiz_times_played`  INT NOT NULL ,
+    `quiz_average_score` FLOAT NOT NULL ,
+    `quiz_best_score`    INT NOT NULL ,
+    `languages_visited`  INT NOT NULL ,
 
-PRIMARY KEY (`email`)
+    PRIMARY KEY (`email`)
 );
