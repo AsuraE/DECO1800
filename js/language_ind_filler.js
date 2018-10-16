c0a0e6500b4d06d997e2e7b046d7703211ed1648
s4461269 <svassilyeva@yahoo.com.au>
2018-10-16 13:54:38 +1000

    javaScript for languges-draft

---
+++ js/language_ind_filler.js
@@ -0,0 +1,45 @@
+/*$(document).ready(get_parsed_data());*/
+$(document).ready(function() {
+	alert("Document is ready");
+)}; 
+
+/*
+
+var location_array= new Array(); 
+var language_array= new Array (); 
+var places_array = new Array();   
+
+
+
+var language = localStorage.getItem("language");
+
+location_array["Yugara"]="Yugara a tribe of Australian Aboriginal people which inhabited the territories from 
+Moreton Bay to Toowoomba including the city of Brisbane 
+(including Ipswich) before European settlement of Australia.
+This group is one of the traditional custodians of the land over which much of Brisbane is built"; 
+
+language_array["Yugara"]="Yugara is classified as belonging to the Durubalic subgroup of the Pama–Nyungan languages, 
+but is also treated as the general name for the languages of the Brisbane area of which Turrbal 
+has historically been considered a dialect.[4] The Australian English word 'yakka' 
+(loosely meaning 'work', as in 'hard yakka') 
+came from the Jagera language (yaga, 'strenuous work')."
+
+places_array["Yugara"]="Map of Traditional Lands of Australian Aboriginals around Brisbane.
+Meebatboogan, Mount Greville, Moogerah Peaks National Park.
+Cooyinnirra, Mount Mitchell, Main Range National Park.
+Booroongapah, Flinders Peak, Teviot Range.
+Ginginbaar, Mount Blaine, Teviot Range."
+
+
+ 
+
+var locationHTML = "<h1>location</h1><p>location_array["Yugara"]</p>";
+var languageHTML = "<h1>language</h1><p>langauge_array["Yugara"]</p>";
+var placesHTML = "<h1>places</h1><p>places_array["Yugara"]</p>"
+
+document.getElementById("location").innerHTML = locationHTML;
+document.getElementById("language").innerHTML = languageHTML;
+document.getElementById("places").innerHTML = placesHTML; */
+
+
+
