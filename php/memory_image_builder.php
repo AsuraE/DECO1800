d53b8a1173d75cb4a27b2cfe75255b1c0a582257
Connor <connor.cooper@uqconnect.edu.au>
2018-10-23 10:22:09 +1000

    Fixed image locations

--- php/memory_image_builder.php
+++ php/memory_image_builder.php
@@ -12,30 +12,31 @@
     Save as smaller image (400x400 should do)
 */
     ini_set('display_errors', 1);
-error_reporting(E_ALL);
+    error_reporting(E_ALL);
     //Set the Content Type
-      // header('Content-type: image/jpeg');
+    //header('Content-type: image/jpeg');
 
-      // Create Image From Existing File
-      $png_image = imagecreatefrompng('../images/memory_game/crow.png');
+    // Create Image From Existing File
+    $png_image = imagecreatefrompng('../images/memory_game/crow.png');
 
-      // Allocate A Color For The Text
-      $black = imagecolorallocate($png_image, 0, 0, 0);
+    // Allocate A Color For The Text
+    $black = imagecolorallocate($png_image, 0, 0, 0);
 
-      // Set Text to Be Printed On Image
-      $text = "Crow";
-      $x = 75;
-      $y = 300;
-      $size = 20;
-      $angle = 0;
-      $font = '..\fonts\consola.ttf';
+    // Set Text to Be Printed On Image
+    $text = "Crow";
+    $x = 100;
+    $y = 1400;
+    $size = 100;
+    $angle = 0;
+    $font = __DIR__.'\consola.ttf';
+    // echo $font;
 
-      // Print Text On Image
-      imagettftext($png_image, $size, $angle, $x, $y, $black, $font, $text);
+    // Print Text On Image
+    imagettftext($png_image, $size, $angle, $x, $y, $black, $font, $text);
 
-      // Send Image to Browser
-      imagejpeg($png_image);
+    // save image to disk
+    imagejpeg($png_image, '../images/memory_game/crowind.jpg');
 
-      // Clear Memory
-      imagedestroy($png_image);
-    ?> 
+    // Clear Memory
+    imagedestroy($png_image);
+?> 
