9ec0961870ce886ac286f305a81120477a7c1aed
DESKTOP-PINNACLEENTHOO\Connor <connor.cooper@uqconnect.edu.au>
2018-10-24 21:06:23 +1000

    Fixed scaling issues on body figure, Improved accuracy of hover locations
    TODO: Change hover text to words from language selected.

--- js/body_figure.js
+++ js/body_figure.js
@@ -1,28 +1,35 @@
 function buildFigure() {
     var headerHTML = `<img src="images/body.png" id="figure" usemap="#body_figure">
                       <map name="body_figure">
-                          <area data-toggle="tooltip" title="Arm" class="arm" shape="circle" coords="906,673,80">
-                          <area data-toggle="tooltip" title="Belly" class="belly" shape="circle" coords="755,765,80">
-                          <area data-toggle="tooltip" title="Ear" class="ear" shape="circle" coords="630,412,80">
-                          <area data-toggle="tooltip" title="Eye" class="eye" shape="circle" coords="800,392,80">
-                          <area data-toggle="tooltip" title="Finger" class="finger" shape="circle" coords="498,673,80">
-                          <area data-toggle="tooltip" title="Foot" class="foot" shape="circle" coords="589,1417,80">
-                          <area data-toggle="tooltip" title="Forehead" class="forehead" shape="circle" coords="781,333,80">
-                          <area data-toggle="tooltip" title="Hand" class="hand" shape="circle" coords="526,926,80">
-                          <area data-toggle="tooltip" title="Head" class="head" shape="circle" coords="728,239,80">
-                          <area data-toggle="tooltip" title="Knee" class="knee" shape="circle" coords="639,1171,80">
-                          <area data-toggle="tooltip" title="Leg" class="leg" shape="circle" coords="686,1080,80">
-                          <area data-toggle="tooltip" title="Mouth" class="mouth" shape="circle" coords="755,468,80">
-                          <area data-toggle="tooltip" title="Neck" class="neck" shape="circle" coords="784,507,80">
-                          <area data-toggle="tooltip" title="Nose" class="nose" shape="circle" coords="741,427,80">
-                          <area data-toggle="tooltip" title="Shin" class="shin" shape="circle" coords="649,1256,80">
-                          <area data-toggle="tooltip" title="Shoulder" class="shoulder" shape="circle" coords="865,550,80">
+                          <area data-toggle="tooltip" title="Arm" class="arm" shape="circle" coords="906,673,50">
+                          <area data-toggle="tooltip" title="Arm" class="arm" shape="circle" coords="608,673,50">
+                          <area data-toggle="tooltip" title="Belly" class="belly" shape="circle" coords="755,765,50">
+                          <area data-toggle="tooltip" title="Ear" class="ear" shape="circle" coords="630,412,20">
+                          <area data-toggle="tooltip" title="Ear" class="ear" shape="circle" coords="860,412,20">
+                          <area data-toggle="tooltip" title="Eye" class="eye" shape="circle" coords="800,392,20">
+                          <area data-toggle="tooltip" title="Eye" class="eye" shape="circle" coords="682,392,20">
+                          <area data-toggle="tooltip" title="Finger" class="finger" shape="circle" coords="498,673,20">
+                          <area data-toggle="tooltip" title="Foot" class="foot" shape="circle" coords="589,1417,50">
+                          <area data-toggle="tooltip" title="Foot" class="foot" shape="circle" coords="906,1417,50">
+                          <area data-toggle="tooltip" title="Forehead" class="forehead" shape="circle" coords="781,333,20">
+                          <area data-toggle="tooltip" title="Hand" class="hand" shape="circle" coords="526,926,50">
+                          <area data-toggle="tooltip" title="Head" class="head" shape="circle" coords="728,239,20">
+                          <area data-toggle="tooltip" title="Knee" class="knee" shape="circle" coords="639,1171,30">
+                          <area data-toggle="tooltip" title="Knee" class="knee" shape="circle" coords="869,1171,30">
+                          <area data-toggle="tooltip" title="Leg" class="leg" shape="circle" coords="686,1080,50">
+                          <area data-toggle="tooltip" title="Leg" class="leg" shape="circle" coords="850,1080,50">
+                          <area data-toggle="tooltip" title="Mouth" class="mouth" shape="circle" coords="755,468,20">
+                          <area data-toggle="tooltip" title="Neck" class="neck" shape="circle" coords="750,507,20">
+                          <area data-toggle="tooltip" title="Nose" class="nose" shape="circle" coords="741,427,20">
+                          <area data-toggle="tooltip" title="Shin" class="shin" shape="circle" coords="649,1256,50">
+                          <area data-toggle="tooltip" title="Shin" class="shin" shape="circle" coords="850,1256,50">
+                          <area data-toggle="tooltip" title="Shoulder" class="shoulder" shape="circle" coords="865,550,60">
+                          <area data-toggle="tooltip" title="Shoulder" class="shoulder" shape="circle" coords="650,550,60">
                       </map>
 
 
     `;
     $(".bodyContent").html(headerHTML);
-    console.log($("#figure").clientWidth);
     $('map').imageMapResize();
     
 }
